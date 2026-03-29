382dc9607cab5db6d848a164805f6b94486b3832
K C Suresh Naidu <sur3sh.naidu@gmail.com>
2026-03-29 21:50:48 +0530

    added comment to avoid confusion

--- .vscode/test_calender.py
+++ .vscode/test_calender.py
@@ -1,4 +1,4 @@
 import calendar
-year=int(input("enter the year: "))
-month=int(input("enter the month: "))
+year=int(input("enter the year: ")) 
+month=int(input("enter the month: ")) #enter the month as a number (1-12)
 print(calendar.month(year,month))
