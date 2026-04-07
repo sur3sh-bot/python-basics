2ffbbbb6078ed0d5fd7ba71d48942e357591d6f1
K C Suresh Naidu <sur3sh.naidu@gmail.com>
2026-04-07 23:14:59 +0530

    added comments

--- .vscode/test_calender.py
+++ .vscode/test_calender.py
@@ -1,4 +1,4 @@
-import calendar
+import calendar #import calendar module to work with calendar related functions
 year=int(input("enter the year: ")) 
 month=int(input("enter the month: ")) #enter the month as a number (1-12)
 print(calendar.month(year,month))
